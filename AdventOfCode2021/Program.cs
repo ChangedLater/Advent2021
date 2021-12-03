2f13fc4b99edf96c60461996277acf8bdc45ca41
Adrian Blackburn <ad.blackburn@me.com>
2021-12-04 09:37:49 +1030

    Day3

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2021;
 
-Console.WriteLine("Starting");
-Day2.CalculatePosition();
+Day3.Part2(Day3.sampleData);
+Day3.Part2(Day3.realData);
 
-Console.WriteLine("Starting P2");
-Day2.CalculatePosition_Part2();
+//Day3.Part1(Day3.sampleData);
+//Day3.Part1(Day3.realData);
+
+//Console.WriteLine("Starting");
+//Day2.CalculatePosition();
+
+//Console.WriteLine("Starting P2");
+//Day2.CalculatePosition_Part2();
 
 //Console.WriteLine("Input");
 //Day1.CalcChanges();
