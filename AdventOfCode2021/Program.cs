638702c86a3823a5e23ec255ac88da77fd2034cd
Adrian Blackburn <ad.blackburn@me.com>
2021-12-07 21:19:11 +1030

    Days 5 & 7

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,8 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2021;
 
-Day3.Part2(Day3.sampleData);
-Day3.Part2(Day3.realData);
+Day5.Part2(Day5.sampleData);
+Day5.Part2(Day5.realData);
+
+//Day5.Part1(Day5.sampleData);
+//Day5.Part1(Day5.realData);
+
+//Day7.Part2(Day7.sampleData);
+//Day7.Part2(Day7.realData);
+
+//Day7.Part1(Day7.sampleData);
+//Day7.Part1(Day7.realData);
+
+//Day3.Part2(Day3.sampleData);
+//Day3.Part2(Day3.realData);
 
 //Day3.Part1(Day3.sampleData);
 //Day3.Part1(Day3.realData);
